43c74f6007c2de578882c1ee1cd101c49fa0f3cb
Lenin Freire <lefreire@espol.edu.ec>
2020-09-13 15:14:36 -0500

    Interfaz grafica

--- src/main/java/ec/edu/espol/bacongameg4/PrimaryController.java
+++ src/main/java/ec/edu/espol/bacongameg4/PrimaryController.java
@@ -1,5 +1,6 @@
 package ec.edu.espol.bacongameg4;
 
+import ec.edu.espol.util.DialogMessage;
 import ec.edu.espol.util.Util;
 import java.io.IOException;
 import java.net.URL;
@@ -7,6 +8,7 @@ import java.util.Deque;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
@@ -22,7 +24,7 @@ public class PrimaryController implements Initializable {
     private VBox dfsBox;
     
     @FXML
-    private VBox dijkstra;
+    private VBox dijkstraBox;
     
     @FXML
     private TextField txtActor1;
@@ -39,20 +41,66 @@ public class PrimaryController implements Initializable {
     private void buscar() throws IOException {
         System.out.println(Util.grafo.vertexes.size());
         
-        Util.grafo.dijkstra(txtActor1.getText());
-        Deque<String> ruta = Util.grafo.caminoMinimo(txtActor1.getText(), txtActor2.getText());
-        while(!ruta.isEmpty())
-            System.out.println(ruta.pop());
-        
-        /*Util.grafo.bfs(txtActor1.getText());
-        ruta = Util.grafo.caminoMinimo(txtActor1.getText(), txtActor2.getText());
-        while(!ruta.isEmpty())
-            System.out.println(ruta.pop());
-        
-        Util.grafo.dfs(txtActor1.getText());
-        ruta = Util.grafo.caminoMinimo(txtActor1.getText(), txtActor2.getText());
-        while(!ruta.isEmpty())
-            System.out.println(ruta.pop());*/
+        if(txtActor1.getText()==null || txtActor2.getText()==null){
+            DialogMessage.nullAlert();
+        }else{
+            if(!rightBox.getChildren().isEmpty())
+                rightBox.getChildren().removeAll();
+            dijkstra();
+            bfs();
+            dfs();
+        }
+    }
+    
+    private void dijkstra(){
+        if(dijkstraBox.getChildren().size()>1){
+                dijkstraBox.getChildren().remove(1,dijkstraBox.getChildren().size());
+            }
+            Long timeStart1=System.currentTimeMillis();
+            Util.grafo.dijkstra(txtActor1.getText());
+            Deque<String> rutaDijkstra = Util.grafo.caminoMinimo(txtActor1.getText(), txtActor2.getText());
+            Label numActores1=new Label("Cantidad: "+rutaDijkstra.size());
+            while(!rutaDijkstra.isEmpty()){
+                System.out.println(rutaDijkstra.pop());
+            }
+            Long timeEnd1=System.currentTimeMillis();
+            Long timeFinal1=timeEnd1-timeStart1;
+            Label time1=new Label("Tiempo: "+timeFinal1);
+            dijkstraBox.getChildren().addAll(numActores1,time1);
+    }
+    
+    private void bfs(){
+        if(bfsBox.getChildren().size()>1){
+                bfsBox.getChildren().remove(1,bfsBox.getChildren().size());
+            }
+            Long timeStart2=System.currentTimeMillis();
+            Util.grafo.bfs(txtActor1.getText());
+            Deque<String> rutaBfs = Util.grafo.caminoMinimo(txtActor1.getText(), txtActor2.getText());
+            Label numActores2=new Label("Cantidad: "+rutaBfs.size());
+            while(!rutaBfs.isEmpty()){
+                System.out.println(rutaBfs.pop());
+            }
+            Long timeEnd2=System.currentTimeMillis();
+            Long timeFinal2=timeEnd2-timeStart2;
+            Label time2=new Label("Tiempo: "+timeFinal2);
+            bfsBox.getChildren().addAll(numActores2,time2);
+    }
+    
+    private void dfs(){
+        if(dfsBox.getChildren().size()>1){
+                dfsBox.getChildren().remove(1,dfsBox.getChildren().size());
+            }
+            Long timeStart3=System.currentTimeMillis();
+            Util.grafo.dfs(txtActor1.getText());
+            Deque<String> rutaDfs = Util.grafo.caminoMinimo(txtActor1.getText(), txtActor2.getText());
+            Label numActores3=new Label("Cantidad: "+rutaDfs.size());
+            while(!rutaDfs.isEmpty()){
+                System.out.println(rutaDfs.pop());
+            }
+            Long timeEnd3=System.currentTimeMillis();
+            Long timeFinal3=timeEnd3-timeStart3;
+            Label time3=new Label("Tiempo: "+timeFinal3);
+            dfsBox.getChildren().addAll(numActores3,time3);
     }
     
 }
