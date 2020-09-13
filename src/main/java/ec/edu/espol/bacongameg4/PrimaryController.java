6901ce9697f1143d173bb79f7ad7ec58b419a887
Lenin Freire <lefreire@espol.edu.ec>
2020-09-13 16:07:40 -0500

    nueva alerta

--- src/main/java/ec/edu/espol/bacongameg4/PrimaryController.java
+++ src/main/java/ec/edu/espol/bacongameg4/PrimaryController.java
@@ -46,7 +46,7 @@ public class PrimaryController {
                 bfs();
                 //dfs();
             }else{
-                DialogMessage.nullAlert();
+                DialogMessage.errorAlert();
             }
         }
     }
--- src/main/java/ec/edu/espol/util/DialogMessage.java
+++ src/main/java/ec/edu/espol/util/DialogMessage.java
@@ -17,9 +17,18 @@ public class DialogMessage {
     
     public static void nullAlert(){
         Alert alert = new Alert(Alert.AlertType.WARNING);
-        alert.setTitle("Error Dialog");
+        alert.setTitle("Warning Dialog");
         alert.setHeaderText("Llenar campos de texto");
         alert.setContentText("Campos de texto vacios, por favor llenar.");
         alert.showAndWait();
     }
+    
+    public static void errorAlert(){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error Dialog");
+        alert.setHeaderText("Error");
+        alert.setContentText("No se han encontrado coincidencias");
+        alert.showAndWait();
+    }
+    
 }
