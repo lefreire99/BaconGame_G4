2fc23e6a41f5456c21cf6e213be5358c731d3d8d
Lenin Freire <lefreire@espol.edu.ec>
2020-09-14 19:08:47 -0500

    cambios

---
+++ src/main/java/ec/edu/espol/util/Constants.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.util;
+
+/**
+ *
+ * @author lfrei
+ */
+public class Constants {
+    public static String TIEMPO="Tiempo: ";
+    public static String CANTIDAD="Cantidad: ";
+    public static String FILEPATH="src/archivos/data.txt";
+}
